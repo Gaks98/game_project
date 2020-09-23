e4a510d35ea114d751fdc6d1733836336a76ad6c
Gaks98 <yvonnegax98@gmail.com>
2020-09-23 21:30:18 +0300

    switch for the power_up_kinds

--- game_project/power_up.cpp
+++ game_project/power_up.cpp
@@ -7,6 +7,34 @@ void load_resources()
   load_bitmap("shield", "shield.png");
   load_bitmap("power", "power.png");
   load_bitmap("potion", "potion-on.png");
-  load_bitmap("diamond-off", "diamond-off.png");
+  load_bitmap("diamond", "diamond.png");
   load_bitmap("star", "star.png");
+}
+
+bitmap light_bitmap(const power_up_data &power_up)
+{
+  string bitmap_name;
+
+  switch (power_up.kind)
+  {
+  case FUEL:
+    bitmap_name = "fuel";
+    break;
+  case SHIELD:
+    bitmap_name = "shield";
+    break;
+  case POWER:
+    bitmap_name = "power";
+    break;
+  case POTION:
+    bitmap_name = "potion";
+    break;
+  case DIAMOND:
+    bitmap_name = "diamond";
+    break;
+  default:
+    bitmap_name = "star";
+  }
+
+  return bitmap_named(bitmap_name);
 }
