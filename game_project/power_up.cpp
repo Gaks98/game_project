42433b7d26ddfd3ccdf539afce9ff538a90a3968
Gaks98 <yvonnegax98@gmail.com>
2020-09-23 21:22:31 +0300

    load_resources()

---
+++ game_project/power_up.cpp
@@ -0,0 +1,12 @@
+#include "power_up.h"
+#include "splashkit.h"
+
+void load_resources()
+{
+  load_bitmap("fuel", "fuel.png");
+  load_bitmap("shield", "shield.png");
+  load_bitmap("power", "power.png");
+  load_bitmap("potion", "potion-on.png");
+  load_bitmap("diamond-off", "diamond-off.png");
+  load_bitmap("star", "star.png");
+}
